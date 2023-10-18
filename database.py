29ad76b27b9a2037703bebeba517f8247076b9e7
yutong13 <yutong13@ualberta.ca>
2023-10-18 17:27:04 +0800

    Implement database.py

--- database.py
+++ database.py
@@ -1,6 +1,26 @@
-import pandas
+import pandas as pd
 
-def data_lanudry(sample_method:str, sample_portion:str,name:str='base.csv'):
+def display_data(sample_method:str=None, sample_portion:str=None,name:str='base.csv', detail= 'short'):
+    '''
+    Display data from csv
+    Display each type and first 5 rows per attribute
+
+    Input:
+    name: the name of csv you want to load
+    sample_method: how do you want to sample from non-fraund records
+    sample_portion: how much we need sample from non-fraud records
+    detail: Print data with 'long': All detail for each column in csv or 'short': a brief summary of data.
+    '''
+    data = data_lanudry( sample_method,sample_portion, name)
+
+    if detail == 'short':
+        print(data)
+    elif detail =='long':
+        for col in data.columns:
+            print(f'Column: {col}')
+            print(data[col].head(5))
+
+def data_lanudry(sample_method:str=None, sample_portion:float=None,name:str='base.csv'):
     '''
     Input:
     name: the name of csv you want to load
@@ -15,22 +35,46 @@ def data_lanudry(sample_method:str, sample_portion:str,name:str='base.csv'):
     '''
 
     # load data:
-    with open(f'./database/{name}') as f:
-        data = f.readlines()
+    data = pd.read_csv(f'./database/{name}')
+
     # we should let pandas to load data as DataFrame object and process the way you want it
-    some_data = data_selection()
+    data = data_selection(data, sample_method, sample_portion)
 
     # return DF object
-    return None
+    return data
 
-def data_selection():
+def data_selection(data,sample_method:str=None, sample_portion:float=None):
     '''
     A method to select data, especially to select from a non-fraud class
     Since we've seen non-fraud records are way more than fraud records
-    '''
-    import random as rd # maybe we can do it with pandas but I have no idea at the moment
-    rd.seed() # we probably need to set a seed at some point in order to replicate some results we want to share
-    
-    # data lanudry e.g. how do you want to sample data?
 
-    return None
+    Input:
+    name: the name of csv you want to load
+    sample_method: how do you want to sample from non-fraund records
+    sample_portion: how much we need sample from non-fraud records
+    '''
+    class_label = 0 # sample non-fraund cases
+    seed = None # the seed you want to replicate result, None by default
+
+    if sample_method == None and sample_portion==None:
+        print("No sample method applys to non-fraud cases.")
+
+    elif sample_method=='random':
+        print(f"Randomly sample {sample_portion*100}% {'non-fraud' if class_label == 0 else 'fraund'} cases")
+        # data lanudry e.g. how do you want to sample data?
+        class_non_fraud = data[data['fraud_bool'] == class_label]
+        sample_non_fraud = class_non_fraud.sample(frac=sample_portion,random_state=seed)
+        data = pd.concat([sample_non_fraud, data[data['fraud_bool'] == reverse_label(class_label)]])
+    return data
+
+def reverse_label(class_label:int)->int:
+    '''
+    Return opposite class label
+    '''
+    if class_label == 1:
+        return 0
+    return 1
+
+
+if __name__ == "__main__":
+    print("Main?")
--- main.py
+++ main.py
@@ -2,6 +2,13 @@
 # import knn
 import database as db
 
+visualize = True # If you want to see all attribute in terminal, False by default
+sample_method = 'random' # Choose between None and 'random'
+sample_portion = 0.4 # Expecting a float number for example: 0.3
+name = 'base.csv'
+# Lenght attribute print data with full details or in brief.
+detail = 'short' # choose between 'short' and 'long'; 'short' by default
+
 def main():
     '''
     This is main logic of our project
@@ -9,6 +16,11 @@ def main():
     TODO: implement models namely, XXX.py and we import them on top
     TODO: After we get results from each model(s) we need to draw them and display results, if any.
     '''
+
+
+    if visualize:
+        db.display_data(sample_method,sample_portion,name, detail)
+
 
 main()
 
