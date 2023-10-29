dcbd8b7e51d768b6b4e4fb1509155e88828d2d5c
xys <xysbysj2020@163.com>
2023-10-29 10:09:36 +0800

    lgbm

--- main.py
+++ main.py
@@ -2,12 +2,12 @@
 # import knn
 import database as db
 
-visualize = False # If you want to see all attribute in terminal, False by default
-sample_method = None # Choose between None and 'random'
-sample_portion = None # Expecting a float number for example: 0.3
+visualize = True # If you want to see all attribute in terminal, False by default
+sample_method = 'random' # Choose between None and 'random'
+sample_portion = 0.01 # Expecting a float number for example: 0.3
 name = 'base.csv'
 # Lenght attribute print data with full details or in brief.
-detail = 'short' # choose between 'short' and 'long'; 'short' by default
+detail = 'long' # choose between 'short' and 'long'; 'short' by default
 
 def main():
     '''
